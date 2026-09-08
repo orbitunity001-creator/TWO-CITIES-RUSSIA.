6b91868b3bab55b9cebfbc4ecc06c68e2f6ad771
orbitunity001-creator <orbitunity001@gmail.com>
2026-09-08 14:18:37 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = "avia-pwa-v2";
+const CACHE_NAME = "avia-v10";
 
-const FILES = [
+const APP_FILES = [
     "./",
     "./index.html",
     "./manifest.json",
@@ -8,80 +8,109 @@ const FILES = [
 ];
 
 
-self.addEventListener("install", event => {
+self.addEventListener(
+    "install",
+    event => {
 
-    self.skipWaiting();
+        self.skipWaiting();
 
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(FILES))
-    );
-
-});
-
-
-self.addEventListener("activate", event => {
-
-    event.waitUntil(
-
-        caches.keys()
-            .then(keys => {
-
-                return Promise.all(
-
-                    keys.map(key => {
-
-                        if (key !== CACHE_NAME) {
-                            return caches.delete(key);
-                        }
-
-                    })
-
-                );
-
-            })
-            .then(() => self.clients.claim())
-
-    );
-
-});
-
-
-self.addEventListener("fetch", event => {
-
-    if (event.request.method !== "GET") {
-        return;
+        event.waitUntil(
+            caches
+                .open(CACHE_NAME)
+                .then(cache => {
+                    return cache.addAll(
+                        APP_FILES
+                    );
+                })
+        );
     }
+);
 
-    event.respondWith(
 
-        fetch(event.request)
-            .then(response => {
+self.addEventListener(
+    "activate",
+    event => {
 
-                if (
-                    response &&
-                    response.status === 200
-                ) {
+        event.waitUntil(
 
-                    const copy = response.clone();
+            caches
+                .keys()
+                .then(keys => {
 
-                    caches.open(CACHE_NAME)
-                        .then(cache => {
-                            cache.put(
-                                event.request,
-                                copy
-                            );
-                        });
+                    return Promise.all(
 
-                }
+                        keys.map(key => {
 
-                return response;
+                            if (
+                                key !== CACHE_NAME
+                            ) {
+                                return caches.delete(
+                                    key
+                                );
+                            }
 
-            })
-            .catch(() => {
-                return caches.match(event.request);
-            })
+                        })
 
-    );
+                    );
+                })
 
-});
+                .then(() => {
+                    return self.clients.claim();
+                })
+
+        );
+    }
+);
+
+
+self.addEventListener(
+    "fetch",
+    event => {
+
+        if (
+            event.request.method !== "GET"
+        ) {
+            return;
+        }
+
+        event.respondWith(
+
+            fetch(event.request)
+
+                .then(response => {
+
+                    if (
+                        response &&
+                        response.status === 200
+                    ) {
+
+                        const copy =
+                            response.clone();
+
+                        caches
+                            .open(CACHE_NAME)
+                            .then(cache => {
+
+                                cache.put(
+                                    event.request,
+                                    copy
+                                );
+
+                            });
+                    }
+
+                    return response;
+
+                })
+
+                .catch(() => {
+
+                    return caches.match(
+                        event.request
+                    );
+
+                })
+
+        );
+    }
+);
