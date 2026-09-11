9ab5c758bc03206b9910a0975b5abf2392e223e7
orbitunity001-creator <orbitunity001@gmail.com>
2026-09-11 17:47:33 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "wingo-v1";
+const CACHE = "nexgram-v1";
 
 const FILES = [
     "./",
@@ -6,57 +6,43 @@ const FILES = [
     "./manifest.json"
 ];
 
+self.addEventListener("install", event => {
 
-self.addEventListener(
-    "install",
-    event => {
+    event.waitUntil(
+        caches.open(CACHE)
+            .then(cache => cache.addAll(FILES))
+    );
 
-        event.waitUntil(
-            caches.open(CACHE_NAME)
-                .then(cache => cache.addAll(FILES))
-        );
-
-        self.skipWaiting();
-    }
-);
+    self.skipWaiting();
+});
 
 
-self.addEventListener(
-    "activate",
-    event => {
+self.addEventListener("activate", event => {
 
-        event.waitUntil(
-            caches.keys().then(keys =>
-                Promise.all(
-                    keys
-                        .filter(key => key !== CACHE_NAME)
-                        .map(key => caches.delete(key))
-                )
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys
+                    .filter(key => key !== CACHE)
+                    .map(key => caches.delete(key))
             )
-        );
+        )
+    );
 
-        self.clients.claim();
-    }
-);
+    self.clients.claim();
+});
 
 
-self.addEventListener(
-    "fetch",
-    event => {
+self.addEventListener("fetch", event => {
 
-        event.respondWith(
-            caches.match(event.request)
-                .then(cached => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(cached => {
 
-                    if (cached) {
-                        return cached;
-                    }
+                return cached ||
+                    fetch(event.request);
 
-                    return fetch(event.request)
-                        .catch(() =>
-                            caches.match("./index.html")
-                        );
-                })
-        );
-    }
-);
+            })
+    );
+
+});
